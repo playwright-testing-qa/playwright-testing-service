ba9414c4f0a1ccdead12ca983919d4d5fce13519
playwright-testing-qa <wintechitllc@gmail.com>
2026-03-25 12:54:32 -0400

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
+  },
+});
