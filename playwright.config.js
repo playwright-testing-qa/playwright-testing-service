b0736d87af542d8ede9a54ac30398b07ef5f51b2
playwright-testing-qa <wintechitllc@gmail.com>
2026-03-29 21:46:59 -0400

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
+  retries: 1,
   use: {
     headless: false,
     launchOptions: {
